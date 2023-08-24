f61d273751f708170ad69da4990e34ae5f52b6b3
phucdinhh <phuc.dinhngoc.7@gmail.com>
2023-08-24 13:34:02 +0700

    state management with zustand

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,14 +4,26 @@ import { Session } from "next-auth";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import Cart from "./Cart";
+import { useCartStore } from "@/store";
+import { AiFillShopping } from "react-icons/ai";
 
 export default function Nav({ user, expires }: Session) {
+  const cartStore = useCartStore();
+
   return (
-    <nav className="flex justify-between items-center py-2">
+    <nav className="flex justify-between items-center py-12">
       <Link href={"/"}>
         <h1>Styled</h1>
       </Link>
-      <ul className="flex items-center">
+      <ul className="flex items-center gap-12 ">
+        {/* If user is not signed in */}
+        <li className="flex items-center text-3xl relative cursor-pointer">
+          <AiFillShopping />
+          <span className="bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center">
+            {cartStore.cart.length}
+          </span>
+        </li>
         {!user && (
           <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign in</button>
@@ -22,14 +34,14 @@ export default function Nav({ user, expires }: Session) {
             <Image
               src={user?.image as string}
               alt={user.name as string}
-              width={48}
-              height={48}
+              width={36}
+              height={36}
               className="rounded-full"
             />
           </li>
         )}
-        <li></li>
       </ul>
+      {cartStore.isOpen && <Cart />}
     </nav>
   );
 }
