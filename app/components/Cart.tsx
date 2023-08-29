db3b139cd91704187ba00b28dbe15f8727cc0a8e
phucdinhh <phuc.dinhngoc.7@gmail.com>
2023-08-29 14:06:58 +0700

    empty carts

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useCartStore } from "@/store";
 import formatPrice from "@/util/PriceFormat";
 import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
+import basket from "@/public/basket.png";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -65,9 +66,16 @@ export default function Cart() {
             </div>
           </div>
         ))}
-        <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
-          Checkout
-        </button>
+        {cartStore.cart.length > 0 ? (
+          <button className="py-2 mt-4 bg-teal-700 w-full rounded-md text-white">
+            Checkout
+          </button>
+        ) : (
+          <div className="flex flex-col items-center gap-12 text-2xl font-medium pt-56 opacity-75">
+            <h1>Uhhh Ohhhh..... It's empty</h1>
+            <Image src={basket} alt={"empty cart"} width={200} height={200} />
+          </div>
+        )}
       </div>
     </div>
   );
